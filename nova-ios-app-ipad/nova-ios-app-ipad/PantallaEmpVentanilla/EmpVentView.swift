9ac202b7b9b4e67fdcf6b11bdbb53db1099b32a0
Luis Garza <97556340+Luisg0527@users.noreply.github.com>
2025-09-22 18:29:43 -0600

    Principio de pantalla

--- nova-ios-app-ipad/nova-ios-app-ipad/PantallaEmpVentanilla/EmpVentView.swift
+++ nova-ios-app-ipad/nova-ios-app-ipad/PantallaEmpVentanilla/EmpVentView.swift
@@ -9,7 +9,60 @@ import SwiftUI
 
 struct EmpVentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            
+            Text("Turno #")
+                .font(.system(size: 60, weight: .bold))
+                .foregroundStyle(Color(red: 255/255, green: 153/255, blue: 0/255))
+            
+            Spacer()
+        }
+        Spacer()
+        VStack{
+            HStack{Spacer()}
+            
+            Button(action:{
+                // algo
+            }){Text("Crear turno")
+                    .frame(maxWidth: .infinity)
+                    .padding(20)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 30))
+                    .tint(Color(red: 1/255, green: 104/255, blue: 138/255))
+                    .fontWeight(.semibold)
+            }
+                .frame(width: 500, height: 85)
+                .padding()
+                .background(.white)
+                .cornerRadius(24)
+                //.padding(.bottom, 32)
+            
+            Button(action:{
+                // algo
+            }){Text("Crear turno")
+                    .frame(maxWidth: .infinity)
+                    .padding(20)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 30))
+                    .tint(.white)
+                    .fontWeight(.semibold)
+            }
+                .frame(width: 500, height: 85)
+                .padding()
+                .background(Color(red: 1/255, green: 104/255, blue: 138/255, opacity: 0.0))
+                .cornerRadius(24)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(.white, lineWidth: 2)
+                    
+                )
+                .padding(.top, 32)
+        }
+        .frame(height: 450)
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color(red: 1/255, green: 104/255, blue: 138/255), Color(red: 0/255, green: 66/255, blue: 88/255)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
     }
 }
 
