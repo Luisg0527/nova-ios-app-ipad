40baf6aa8c4eb5653944f7741b0a81c47fd6c1c1
Luis Garza <97556340+Luisg0527@users.noreply.github.com>
2025-09-22 18:31:34 -0600

    Create InicioSesionView.swift

---
+++ nova-ios-app-ipad/nova-ios-app-ipad/Views/InicioSesionView.swift
@@ -0,0 +1,18 @@
+//
+//  InicioSesionView.swift
+//  nova-ios-app-ipad
+//
+//  Created by Luis Garza on 22/09/25.
+//
+
+import SwiftUI
+
+struct InicioSesionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InicioSesionView()
+}
